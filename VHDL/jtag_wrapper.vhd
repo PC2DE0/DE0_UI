e5216a29961f46de1651df14c4082b2b14005dda
Jayson <jaysalkey@gmail.com>
2016-05-03 20:54:56 -0400

    changed output from in_stdlv to out_stdlv

--- VHDL/jtag_wrapper.vhd
+++ VHDL/jtag_wrapper.vhd
@@ -6,12 +6,12 @@ entity jtag_wrapper is
     ready : out std_logic;
     input : in std_logic_vector(width-1 downto 0); -- Data from FPGA to Computer
     input_valid : in std_logic;
-    output : in std_logic_vector(width-1 downto 0); -- Data from Computer to FPGA
+    output : out std_logic_vector(width-1 downto 0); -- Data from Computer to FPGA
     output_valid : out std_logic --asserted for one cycle for every output, asserted once every width cycles?
   );
 end jtag_wrapper;
 
 
 architecture bhvr of jtag_wrapper is
-  
+
 end bhvr;
